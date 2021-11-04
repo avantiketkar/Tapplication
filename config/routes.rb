bcd655f1cabe4b541e15951d001a2ace7c926adc
Avanti Ketkar <avanti.ketkar@shopify.com>
2021-11-04 15:37:52 -0700

    Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   resources :articles
   root 'pages#home'
   get 'about', to:'pages#about'
+  get 'signup', to:'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_203000) do
+ActiveRecord::Schema.define(version: 2021_10_31_142210) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
